e31ce9f16ff18ea0508c62e2c43a7ef234a9f3b2
ChernyshevaNetology <chernyshevak87@mail.ru>
2025-01-15 17:39:42 +0200

    feat: added theme data to render multiple themes

---
+++ src/projects/project-example/multiple-theme-render.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// сюда импортировать дату
+// сюда импортировать компонент theme которые мы создали с тобой
+
+export const MultipleThemeRender = () => {
+    return (
+        <div>
+            {
+                // здесь мэпом пройтись по theme-data и вывести <Theme /> передавая в theme нужные пропсы
+                // в app.jsx уже отрендерить <MultipleThemeRender /> в multiple counters пример есть, как мэп в реакте работает
+            }
+        </div>
+    );
+};
--- src/projects/project-example/theme.jsx
+++ src/projects/project-example/theme.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-export const Theme = ({name, description, link, image, blocked}) => {
+export const Theme = ({name, description, link, image}) => {
     return (
         <div className={'theme'}>
             <h2>{name}</h2>
             <p>{description}</p>
             <a href={link}>Смотреть тему</a>
             <img src={image} />
-            <button disabled={blocked}>Button</button>
         </div>
     );
 };
