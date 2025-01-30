be4ebc33f89cbf6dd777ba02c31b603627f6dee1
Vito <example@gmail.com>
2025-01-30 21:48:55 +0600

    Rewritten code made functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import { MultipleThemeRender } from './projects/project-example/multiple-theme-render'
 import { MultipleCounters } from "./projects/project-2/multiple-counters";
+import { Counter } from "./projects/project-1/counter";
 
 export const App = () => {
     return (
     <div className={'container'}>
-        <MultipleCounters />
+        <Counter />
     </div>
   )
 }
--- src/projects/project-1/counter.jsx
+++ src/projects/project-1/counter.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
-import { Button } from '../../components/button';
-import { Notice } from '../../components/notice';
+import { Flex, Button, Badge, Checkbox, Divider, Alert, Stack } from '@mantine/core';
+import { IconInfoCircle } from '@tabler/icons-react';
 
 export const Counter = () => {
     const [count, setCount] = useState(0);
+    const [checked, setChecked] = useState(false);
 
+    const handleCheckboxState = () => setChecked(!checked);
     const onIncrement = () => setCount((prev) => prev + 1);
     const onDecrement = () => setCount((prev) => prev - 1);
     const onReset = () => setCount( 0);
@@ -13,24 +15,56 @@ export const Counter = () => {
         if (count >= 6 && count <= 15) return "green";
         if (count > 15) return "red";
     }
-
+    const icon = <IconInfoCircle />
     return (
         <>
-            <div>
-
-                <Button title={"increment"} handler={onIncrement}/>
-                <Button blocked={count <= 0} title={"decrement"} handler={onDecrement}/>
-                <Button blocked={count === 0} title={"reset"} handler={onReset}/>
-            </div>
-            <div className="count">
+            <Flex
+                mih={50}
+                gap="md"
+                justify="center"
+                align="center"
+                direction="row"
+                wrap="wrap"
+            >
+            <Checkbox onChange={handleCheckboxState} label="Display counters"/>
+                </Flex>
+            <Divider my="md" />
+            { checked ?
+                (<Flex
+                    mih={50}
+                    gap="md"
+                    justify="center"
+                    align="center"
+                    direction="row"
+                    wrap="wrap"
+                >
+                    <Button onClick={onIncrement}>Increment</Button>
+                    <Button onClick={onDecrement}>Decrement</Button>
+                    <Button onClick={onReset}>Reset</Button>
+                    <Badge size="xl" color="cyan" circle>{count}</Badge>
+                </Flex>) : null
+            }
+{/*            <div className="count">
                 <h3 className={limiter(count)}>
                     {count}
                 </h3>
-            </div>
-            <div>
-                <Notice warning={(count >= 15) ? 'Счетчик не может превышать 15' : null} />
-            </div>
-
+            </div>*/}
+            <Divider my="md" />
+                { count > 10 ?
+                    (
+                        <Stack
+                            h={300}
+                            bg="var(--mantine-color-body)"
+                            align="center"
+                            justify="center"
+                            gap="md"
+                        >
+                    <Alert variant="light" color="blue" title="Внимание" icon={icon}>
+                        Счетчик не может превышать 10
+                    </Alert>
+                            </Stack>
+                    ) : null
+                }
         </>
     );
 };
