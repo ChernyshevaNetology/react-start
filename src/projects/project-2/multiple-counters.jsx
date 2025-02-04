f8a5a92608dcbaa03d7e4d45b3c91e852f73f9eb
Vito <example@gmail.com>
2025-02-04 12:41:07 +0600

    Modal window issue

--- src/projects/project-2/multiple-counters.jsx
+++ src/projects/project-2/multiple-counters.jsx
@@ -1,98 +1,114 @@
-import React, { useState } from 'react';
-import { SingleCounter } from './single-counter';
-import {Flex, Switch, Stack, Divider, Button} from '@mantine/core';
+import React, { useState } from "react";
+import { SingleCounter } from "./single-counter";
+import {
+  Flex,
+  Switch,
+  Stack,
+  Divider,
+  Button,
+  Group,
+  Modal,
+  useModalsStack,
+} from "@mantine/core";
 
 const data = [
-    {id: 1, value: 0},
-    {id: 2, value: 3},
-    {id: 3, value: 4},
-    {id: 4, value: 0},
-    {id: 5, value: 1},
-]
+  { id: 1, value: 0 },
+  { id: 2, value: 3 },
+  { id: 3, value: 4 },
+  { id: 4, value: 0 },
+  { id: 5, value: 1 },
+];
 
 export const MultipleCounters = () => {
-    const [counters, setCounters] = useState(data);
-    const [checked, setChecked] = useState(false);
+  const [counters, setCounters] = useState(data);
+  const [checked, setChecked] = useState(false);
 
-    const onIncrement = (id) => {
-        const updatedCounters = counters.map((counter) => {
-            if (counter.id === id) {
-                return {
-                    ...counter,
-                    value: ++counter.value,
-                }
-            }
-            return counter;
-        })
-        setCounters(updatedCounters);
-    }
-    const onDecrement = (id) => {
-        const updatedCounters = counters.map((counter) => {
-            if (counter.id === id) {
-                return {
-                    ...counter,
-                    value: --counter.value,
-                }
-            }
-            return counter;
-        })
-        setCounters(updatedCounters);
-    }
+  const onIncrement = (id) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === id) {
+        return {
+          ...counter,
+          value: ++counter.value,
+        };
+      }
+      return counter;
+    });
+    setCounters(updatedCounters);
+  };
+  const onDecrement = (id) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === id) {
+        return {
+          ...counter,
+          value: --counter.value,
+        };
+      }
+      return counter;
+    });
+    setCounters(updatedCounters);
+  };
 
-    const onReset = (id) => {
-        const updatedCounters = counters.map((counter) => {
-            if (counter.id === id) {
-                return {
-                    ...counter,
-                    value: 0,
-                }
-            }
-            return counter;
-        })
-        setCounters(updatedCounters);
-    }
+  const onReset = (id) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === id) {
+        return {
+          ...counter,
+          value: 0,
+        };
+      }
+      return counter;
+    });
+    setCounters(updatedCounters);
+  };
 
-    const onMasterReset = (id) => {
-        const updatedCounters = counters.map((counter) => {
-                return {
-                    ...counter,
-                    value: 0,
-                }
-           })
-        setCounters(updatedCounters);
-    }
+  const onMasterReset = () => {
+    const updatedCounters = counters.map((counter) => {
+      return {
+        ...counter,
+        value: 0,
+      };
+    });
+    setCounters(updatedCounters);
+  };
 
-    const handleSwitchState = () => setChecked(!checked);
+  const handleSwitchState = () => setChecked(!checked);
 
-    return (
-        <>
-            <Flex
-                mih={50}
-                gap="md"
-                justify="center"
-                align="center"
-                direction="row"
-                wrap="wrap"
-            >
-                <Switch onChange={handleSwitchState} label="Активировать счетчики"/>
-            </Flex>
-            <Divider my="md" />
-            {checked ? (<Stack
-                mih={50}
-                gap="md"
-                justify="center"
-                align="center"
-                direction="row"
-                wrap="wrap"
-            >
-                {
-                    counters.map((counter) => <SingleCounter id={counter.id} onIncrement={onIncrement}
-                                                             onDecrement={onDecrement} onReset={onReset}
-                                                             count={counter.value}/>)
-                }
-                    <Button onClick={onMasterReset}>MasterReset</Button>
-                </Stack>) : null
-            }
-        </>
-    );
-}
+  return (
+    <>
+      <Flex
+        mih={50}
+        gap="xs"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        <Switch onChange={handleSwitchState} label="Активировать счетчики" />
+      </Flex>
+      <Divider my="md" />
+      {checked ? (
+        <Stack
+          mih={50}
+          gap="xs"
+          justify="center"
+          align="center"
+          direction="row"
+          wrap="wrap"
+        >
+          {counters.map((counter) => (
+            <SingleCounter
+              id={counter.id}
+              onIncrement={onIncrement}
+              onDecrement={onDecrement}
+              onReset={onReset}
+              count={counter.value}
+            />
+          ))}
+          <Button color="red" onClick={onMasterReset}>
+            MasterReset
+          </Button>
+        </Stack>
+      ) : null}
+    </>
+  );
+};
--- src/projects/project-2/single-counter.jsx
+++ src/projects/project-2/single-counter.jsx
@@ -1,22 +1,88 @@
-import React from 'react';
-import {Button, Badge, Flex, Stack, Switch, Divider} from '@mantine/core';
+import React from "react";
+import {
+  Button,
+  Badge,
+  Flex,
+  Stack,
+  Switch,
+  Divider,
+  Group,
+  Modal,
+  useModalsStack,
+} from "@mantine/core";
 
-
-
-export const SingleCounter = ({count, onIncrement, onDecrement, onReset, id}) => {
-        return(
-            <Flex
-                mih={50}
-                gap="md"
-                justify="center"
-                align="center"
-                direction="row"
-                wrap="wrap"
+export const SingleCounter = ({
+  count,
+  onIncrement,
+  onDecrement,
+  onReset,
+  id,
+}) => {
+  const stack = useModalsStack([
+    "delete-page",
+    "confirm-action",
+    "really-confirm-action",
+  ]);
+  return (
+    <Flex
+      mih={50}
+      gap="xs"
+      justify="center"
+      align="center"
+      direction="row"
+      wrap="wrap"
+    >
+      <Button
+        variant="filled"
+        onClick={() => {
+          onIncrement(id);
+        }}
+      >
+        Increment
+      </Button>
+      <Button
+        variant="filled"
+        onClick={() => {
+          onDecrement(id);
+        }}
+      >
+        Decrement
+      </Button>
+      <Button
+        variant="filled"
+        onClick={() => {
+          onReset(id);
+        }}
+      >
+        Reset
+      </Button>
+      <Modal.Stack>
+        <Modal {...stack.register("confirm-action")} title="Delete button">
+          Are you sure you want to perform this action? This action cannot be
+          undone. If you are sure, press confirm button below.
+          <Group mt="lg" justify="flex-end">
+            <Button onClick={stack.closeAll} variant="default">
+              Cancel
+            </Button>
+            <Button
+              onClick={() => stack.open("really-confirm-action")}
+              color="red"
             >
-    <Button variant="filled" onClick={() => {onIncrement(id)}}>Increment</Button>
-    <Button variant="filled" onClick={() => {onDecrement(id)}}>Decrement</Button>
-    <Button variant="filled" onClick={() => {onReset(id)}}>Reset</Button>
-    <Badge size="xl" circle>{count}</Badge>
+              Confirm
+            </Button>
+          </Group>
+        </Modal>
+      </Modal.Stack>
+      <Button
+        variant="filled"
+        color="orange"
+        onClick={() => stack.open("delete-page")}
+      >
+        Trash bin
+      </Button>
+      <Badge size="xl" circle>
+        {count}
+      </Badge>
     </Flex>
-            )
-}
+  );
+};
