42ca0bfba4aea3704ae78bbe7d62ee7adf53c755
ChernyshevaNetology <chernyshevak87@mail.ru>
2025-03-01 12:24:01 +0200

    added info for services

---
+++ src/projects/project-3/form-data.js
@@ -0,0 +1,66 @@
+export const preSelectedProducts = [
+  {
+    name: "Лицензия",
+    price: 9990,
+    selected: true,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+  {
+    name: "Месяц поддержки",
+    selected: true,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+  {
+    name: "Шаблон",
+    selected: true,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+  {
+    name: "41 плагинов",
+    selected: true,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+  {
+    name: "Установка",
+    selected: true,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+  {
+    name: "Смена копирайтов",
+    selected: true,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+];
+
+export const additionalServices = [
+  {
+    name: "Премиальные темы",
+    price: 7990,
+    selected: false,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+  {
+    name: "iOS приложение",
+    price: 9990,
+    selected: false,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+  {
+    name: "Android приложение",
+    price: 9990,
+    selected: false,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+  {
+    name: "Базовый пакет услуг SEO",
+    price: 7990,
+    selected: false,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+  {
+    name: "Импорт базы данных",
+    price: 19990,
+    selected: false,
+    toolTipText: "Dummy Tooltip text to display as popup when clicked",
+  },
+];
