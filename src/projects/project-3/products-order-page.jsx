70200ad3d0167c710615f6097076a1ec3c98ba04
Vito <example@gmail.com>
2025-03-10 12:12:25 +0600

    Popover issue

--- src/projects/project-3/products-order-page.jsx
+++ src/projects/project-3/products-order-page.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-import { Button, Checkbox, Group, TextInput, Flex } from "@mantine/core";
+import {
+  Button,
+  Checkbox,
+  Group,
+  TextInput,
+  Flex,
+  Select,
+  Popover,
+  Badge,
+  Fieldset,
+  Space,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
 
 // это общая страница продуктов она будет строится из различных компонентов и форм
@@ -15,12 +26,11 @@ export const ProductsOrderPage = () => {
     },
 
     validate: {
-      name: (value) => (value ? null : "Invalid name"),
-      lastName: (value) => (value ? null : "Invalid last name"),
-      email: (value) =>
-        /^\S+@\S+$/.test(value) ? null : "Invalid email address",
-      termsOfService: (value) =>
-        value ? null : "You have not checked the box",
+      product: (value) => (value ? null : "Выберите продукт"),
+      name: (value) => (value ? null : "Неправильное имя"),
+      lastName: (value) => (value ? null : "Неправильная фамилия"),
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Неправильный email"),
+      termsOfService: (value) => (value ? null : "Вы не отметили поле"),
     },
   });
 
@@ -35,28 +45,109 @@ export const ProductsOrderPage = () => {
         wrap="wrap"
       >
         <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <Group gap="lg">
+            <Select
+              withAsterisk
+              label="Скрипт"
+              placeholder="Выберите продукт"
+              data={[
+                "Real Estate",
+                "Classifieds Ads",
+                "Cars",
+                "Pets",
+                "Services",
+                "Boats",
+              ]}
+              key={form.key("product")}
+              {...form.getInputProps("product")}
+            />
+            <Popover width={200} position="bottom" withArrow shadow="md">
+              <Popover.Target>
+                <Badge size="xl" circle>
+                  ?
+                </Badge>
+              </Popover.Target>
+              <Popover.Dropdown>
+                <Text size="xs">
+                  В данном поле вы можете выбрать тип скрипта, который будет
+                  установлен на ваш сервер.
+                </Text>
+              </Popover.Dropdown>
+            </Popover>
+          </Group>
           <TextInput
             withAsterisk
-            label="Name"
-            placeholder="Your name"
+            label="Имя"
+            placeholder="Ваше имя"
             key={form.key("name")}
             {...form.getInputProps("name")}
           />
           <TextInput
             withAsterisk
-            label="Last Name"
-            placeholder="Your last name"
+            label="Фамилия"
+            placeholder="Ваша фамилия"
             key={form.key("lastName")}
             {...form.getInputProps("lastName")}
           />
           <TextInput
             withAsterisk
             label="Email"
-            placeholder="Enter email"
+            placeholder="Введите email"
             key={form.key("email")}
             {...form.getInputProps("email")}
           />
-
+          <Fieldset legend="Дополнения и услуги">
+            <Group gap="lg">
+              <Checkbox labelPosition="left" label="Премиальные темы" />
+              <Popover width={200} position="bottom" withArrow shadow="md">
+                <Popover.Target>
+                  <Badge size="xl" circle>
+                    ?
+                  </Badge>
+                </Popover.Target>
+                <Popover.Dropdown>
+                  <Text size="xs">
+                    В данном поле вы можете выбрать тип скрипта, который будет
+                    установлен на ваш сервер.
+                  </Text>
+                </Popover.Dropdown>
+              </Popover>
+            </Group>
+            <Space h="xl" />
+            <Group gap="lg">
+              <Checkbox labelPosition="left" label="iOS приложение" />
+              <Popover width={200} position="bottom" withArrow shadow="md">
+                <Popover.Target>
+                  <Badge size="xl" circle>
+                    ?
+                  </Badge>
+                </Popover.Target>
+                <Popover.Dropdown>
+                  <Text size="xs">
+                    В данном поле вы можете выбрать тип скрипта, который будет
+                    установлен на ваш сервер.
+                  </Text>
+                </Popover.Dropdown>
+              </Popover>
+            </Group>
+            <Space h="xl" />
+            <Group gap="lg">
+              <Checkbox labelPosition="left" label="Android приложение" />
+              <Popover width={200} position="bottom" withArrow shadow="md">
+                <Popover.Target>
+                  <Badge size="xl" circle>
+                    ?
+                  </Badge>
+                </Popover.Target>
+                <Popover.Dropdown>
+                  <Text size="xs">
+                    В данном поле вы можете выбрать тип скрипта, который будет
+                    установлен на ваш сервер.
+                  </Text>
+                </Popover.Dropdown>
+              </Popover>
+            </Group>
+          </Fieldset>
           <Checkbox
             mt="md"
             label="Я принимаю условия публичной оферты"
@@ -68,7 +159,6 @@ export const ProductsOrderPage = () => {
             <Button type="submit">Оплатить</Button>
           </Group>
         </form>
-        );
       </Flex>
     </div>
   );
