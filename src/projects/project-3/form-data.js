ad735a1fb9e923a953c71df3a74df7eb8664e514
Vito <example@gmail.com>
2025-03-14 19:37:34 +0600

    Two components added

--- src/projects/project-3/form-data.js
+++ src/projects/project-3/form-data.js
@@ -1,32 +1,38 @@
 export const preSelectedProducts = [
   {
     name: "Лицензия",
+    key: "product",
     price: 9990,
     selected: true,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
   {
     name: "Месяц поддержки",
+    key: "support",
     selected: true,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
   {
     name: "Шаблон",
+    key: "freeTemplate",
     selected: true,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
   {
     name: "42 плагина",
+    key: "freePlugins",
     selected: true,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
   {
     name: "Установка",
+    key: "installation",
     selected: true,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
   {
     name: "Смена копирайтов",
+    key: "brandingRemoval",
     selected: true,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
@@ -34,33 +40,33 @@ export const preSelectedProducts = [
 
 export const additionalServices = [
   {
+    key: "themes",
     name: "Премиальные темы",
     price: 7990,
-    selected: false,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
   {
+    key: "ios",
     name: "iOS приложение",
     price: 9990,
-    selected: false,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
   {
+    key: "android",
     name: "Android приложение",
     price: 9990,
-    selected: false,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
   {
+    key: "seo",
     name: "Базовый пакет услуг SEO",
     price: 7990,
-    selected: false,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
   {
+    key: "dbTransfer",
     name: "Импорт базы данных",
     price: 19990,
-    selected: false,
     toolTipText: "Dummy Tooltip text to display as popup when clicked",
   },
 ];
